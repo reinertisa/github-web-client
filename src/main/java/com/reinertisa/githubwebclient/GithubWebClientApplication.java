d30cf4704942ac5db59c88c19118f6f71097953c
Isa Reinert <reinert.isa@gmail.com>
2025-06-12 16:20:54 -0700

    feat(api): initialize project

---
+++ src/main/java/com/reinertisa/githubwebclient/GithubWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.githubwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubWebClientApplication.class, args);
+    }
+
+}
