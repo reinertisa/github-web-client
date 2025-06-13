c9b3fe8a4a3eb464e346cf25ce4241e29dfc9ffe
Isa Reinert <reinert.isa@gmail.com>
2025-06-13 16:45:43 -0700

    feat(api): create Post repository

---
+++ src/main/java/com/reinertisa/githubwebclient/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.reinertisa.githubwebclient.repository;
+
+import com.reinertisa.githubwebclient.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+}
